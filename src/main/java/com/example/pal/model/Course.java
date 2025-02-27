78750a16ae9fd1e44cdc605ab30683377654e3ce
yaircamilo05 <yair.sanchez21369@ucaldas.edu.co>
2025-02-26 22:48:22 -0500

    add Category and Course entity models with relationships

---
+++ src/main/java/com/example/pal/model/Course.java
@@ -0,0 +1,30 @@
+package com.example.pal.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = false)
+    private String title;
+
+    @Column(nullable = false, unique = false)
+    private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    
+    @Column(nullable = false, unique = false)
+    private float price;
+
+    @ManyToOne
+    @JoinColumn(name = "instructor_id", nullable = false)
+    private User instructor;
+}

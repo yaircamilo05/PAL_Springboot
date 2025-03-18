e55cbf61803455582887374131bebdaf74ff583c
yaircamilo05 <yair.sanchez21369@ucaldas.edu.co>
2025-03-17 21:14:52 -0500

    Actualizar la configuración de CORS en SecurityConfig para permitir solo orígenes específicos, mejorando la seguridad de la aplicación.

--- src/main/java/com/example/pal/config/SecurityConfig.java
+++ src/main/java/com/example/pal/config/SecurityConfig.java
@@ -29,10 +29,10 @@ public class SecurityConfig {
 	@Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-        configuration.setAllowedHeaders(List.of("*"));
-        configuration.setExposedHeaders(List.of("*"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
         configuration.setMaxAge(3600L);
         

847cc788cae0945f59e0ece6d41890a9c1bf5dfa
yaircamilo05 <yair.sanchez21369@ucaldas.edu.co>
2025-04-23 19:42:53 -0500

    Add AuthRequest and AuthResponse DTOs; include JWT dependency in build.gradle

---
+++ src/main/java/com/example/pal/dto/AuthRequest.java
@@ -0,0 +1,10 @@
+// AuthRequest.java
+package com.example.pal.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthRequest {
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/example/pal/dto/AuthResponse.java
@@ -0,0 +1,11 @@
+// AuthResponse.java
+package com.example.pal.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
